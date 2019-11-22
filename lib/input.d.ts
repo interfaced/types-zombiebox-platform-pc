3aed7ff76c174b52b850a631baca83fe70c5e3ca
fridge <reefridgerator@gmail.com>
2019-11-22 14:09:45 +0700

    createStatefulVideo

--- lib/input.d.ts
+++ lib/input.d.ts
@@ -3,8 +3,8 @@ import Keys from 'zombiebox/zb/device/input/keys';
 
 export default class Input extends AbstractInput {
   constructor();
-  public isPointingDeviceSupported(): boolean;
-  public enablePointingDevice(): void;
-  public disablePointingDevice(opt_timeout?: number): void;
+  isPointingDeviceSupported(): boolean;
+  enablePointingDevice(): void;
+  disablePointingDevice(opt_timeout?: number): void;
   protected _createKeysMap(): { [key: number]: Keys };
 }
