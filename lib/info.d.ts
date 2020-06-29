1547cd02c895f060caaf43f364704dc0f67102d7
reefridge <reefridgerator@gmail.com>
2020-06-29 16:44:18 +0700

    update types

--- lib/info.d.ts
+++ lib/info.d.ts
@@ -2,14 +2,15 @@ import AbstractInfo from 'zombiebox/zb/device/abstract-info';
 import { Resolution } from 'zombiebox/zb/device/resolutions';
 
 export default class Info extends AbstractInfo {
-  constructor();
-  type(): string;
-  version(): string;
-  manufacturer(): string;
-  model(): string;
-  serialNumber(): string;
-  softwareVersion(): string;
-  hardwareVersion(): string;
-  osdResolutionType(): Resolution;
-  protected _getLocale(): string;
+	constructor();
+	type(): string;
+	version(): string;
+	manufacturer(): string;
+	model(): string;
+	serialNumber(): string;
+	softwareVersion(): string;
+	hardwareVersion(): string;
+	getPanelResolution(): Resolution;
+	osdResolutionType(): Resolution;
+	protected _getLocale(): string;
 }
--- lib/input.d.ts
+++ lib/input.d.ts
@@ -1,10 +1,12 @@
 import AbstractInput from 'zombiebox/zb/device/abstract-input';
-import Keys from 'zombiebox/zb/device/input/keys';
+import Key from 'zombiebox/zb/device/input/key';
 
 export default class Input extends AbstractInput {
-  constructor();
-  isPointingDeviceSupported(): boolean;
-  enablePointingDevice(): void;
-  disablePointingDevice(opt_timeout?: number): void;
-  protected _createKeysMap(): { [key: number]: Keys };
+	constructor();
+	isPointingDeviceSupported(): boolean;
+	enablePointingDevice(): void;
+	disablePointingDevice(opt_timeout?: number): void;
+	eventToKeyCode(e: (KeyboardEvent | WheelEvent)): Key;
+	protected _createKeysMap(): { [key: number]: Key };
+	protected _serviceKeys: number[];
 }
